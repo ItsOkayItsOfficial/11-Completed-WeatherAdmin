0d21c20811a03733671dff29496589bc217fc5db
Alex P <alexp@itsokayitsofficial.io>
2017-09-27 19:09:29 -0700

    Aded new dependencies and logData command to cli.js

--- cli.js
+++ cli.js
@@ -6,3 +6,30 @@
 * URL:
 */
 
+// Variables - Require
+var weather = require("weather-js")
+var fs = require("fs");
+var request = require("request");
+
+// Variables - Global
+var input = process.argv;
+var calc = parseFloat;
+var op = input[2];
+var mod = input[3];
+
+// Run - logData()
+// turn on logData
+logData('', true);
+logData('Command: ' + op + ' ' + mod, true);
+
+// Function - Log input
+function logData(log, print) {
+    // File - logoData logs...data
+    fs.appendFileSync('log.txt', log + '\r\n');
+    // If - on/off for logData
+    if (!print) {
+        console.log(log);
+      } else {
+        console.log(p);
+      }
+    };
